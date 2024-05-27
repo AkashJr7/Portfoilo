ac71154646d069c68bec57d2638420b56bf0ff81
AkashJr7 <133139753+AkashJr7@users.noreply.github.com>
2024-05-28 00:13:21 +0530

    Portfolio
    New Portfolio Application created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img
+          src={"https://avatars.githubusercontent.com/u/25058652"}
+          alt="Founder"
+        />
+
+        <h2>Akash joshi</h2>
+        <p>If you are consistent in adapting a habit nobody can stop u from achieving your goal</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        <article>
+          <a href="https://www.youtube.com/channel/UCLKviQTd9-WuJpJJwHE9-mA" target={"blank"}>
+            <AiFillYoutube />
+          </a>
+          <a href="https://www.linkedin.com/in/akash-joshi-23455222b/" target={"blank"}>
+            <AiFillLinkedin />
+          </a>
+          <a href="https://github.com/AkashJr7" target={"blank"}>
+            <AiFillGithub />
+          </a>
+        </article>
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
