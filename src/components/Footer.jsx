10834018b6eb69f88f57942a96f127b1e9eb0d4f
AkashJr7 <133139753+AkashJr7@users.noreply.github.com>
2025-04-15 14:56:49 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <footer>
       <div>
         <img
-          src={"https://avatars.githubusercontent.com/u/25058652"}
+          src={"logo"}
           alt="Founder"
         />
 
